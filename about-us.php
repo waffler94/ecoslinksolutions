6e9aa64578bd6a55e2e5e3b786bf02fda638d774
harng <wangth94@gmail.com>
2025-07-11 00:47:52 +0800

    commit

--- about-us.php
+++ about-us.php
@@ -369,42 +369,7 @@
           </div>
         </section>
       </main>
-      <footer id="footer">
-        <div class="sm:w-5/6 w-11/12 px-0 pt-16 pb-20 flex lg:flex-nowrap flex-wrap gap-2 justify-between mx-auto">
-            <div class="w-full lg:w-1/3">
-                <a href="/" class="w-full md:w-2/3">
-                    <img
-                    src="eco-logo3.png"
-                    alt="White Logo"
-                    class="mx-auto px-4 sm:px-0 block sm:mx-0 max-w-[220px]"
-                    width="224"
-                    height="124"
-                    />
-                </a>
-                <p class="mt-6 lg:max-w-[500px] text-center sm:text-left">Since 2015, Ecos Link Solutions has been at the forefront of providing comprehensive monitoring and integration solutions for various industries across Malaysia. </p>
-                <a href="https://www.linkedin.com/company/alam-mulia-teknologi/" target="_blank"class="mt-4 block sm:mx-0 mx-auto w-fit hover:text-[#57c43f] transition"><i class="text-[2em] fab fa-linkedin"></i></a>
-            </div>
-            <div class="w-full lg:w-1/5 pt-6 mt-4 md:mt-0">
-                <h6 class="text-[21px] font-bold mb-6">Quick Links</h6>
-                <div class="grid grid-cols-1">
-                  <div>
-                    <a href="/" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>Home</span></a>
-                    <a href="/about-us" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>About Us</span></a>
-                    <a href="/contact-us" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>Contact Us</span></a>
-                  </div>
-                </div>
-            </div>
-            <div class="w-full lg:w-1/3 pt-6 mt-4 md:mt-0">
-                <h6 class="text-[21px] font-bold mb-6">Our Solutions</h6>
-                <a href="/analytical-process-measurement" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>Analytical Process Measurement</span></a>
-                <a href="/process-skid-tab" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>Process Skid</span></a>
-                <a href="/iot-system" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>IOT System</span></a>
-                <a href="/system-integration-work" class="mb-4 block flex gap-x-4 items-center hover:text-[#57c43f]"><i class="fas fa-caret-right"></i><span>System Integration Work</span></a>
-            </div>
-        </div>
-        <a href="https://wa.link/zq82cu" target="_blank" class="flex items-center justify-center text-[1.8em] w-[50px] h-[50px] rounded-full bg-[#57c43f] transition border border-solid border-[#57c43f] text-white fixed right-4 bottom-4"><i class="fab fa-whatsapp"></i></a>
-        <p class="text-center text-[11px] py-6 border-t-[1px] border-solid border-white">&copy; <span class="latestYear"></span> ECOSLINKSOLUTIONS. All rights reserved.</p>
-    </footer>
+      <?php include("footer.php"); ?>
 
     <script>
         AOS.init({once: false});
